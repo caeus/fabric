bea3188196c80009c2061946fbf71f06a30970f3
Alejandro Navas <caeus@Alejandros-MacBook-Pro.local>
2023-12-29 17:32:27 -0500

    ok

---
+++ domains/boilerplate/main.py
@@ -0,0 +1,8 @@
+import domains.bedrock.bedrock.main
+
+print()
+print(domains.bedrock.bedrock.main.asdqwe())
+print("alksdjaslkjdsalkj")
+print("alksdjaslkjdsalkj")
+print("alksdjaslkjdsalkj")
+
---
+++ domains/boilerplate/src/main/main.py
@@ -0,0 +1,5 @@
+import domains.bedrock.src.utils.main
+
+print(domains.bedrock.src.utils.main.some_method())
+
+
